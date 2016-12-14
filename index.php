f7a09e4d902dcd378480ec2811b5350086a7834b
Devin <devinwiebelt@gmail.com>
2016-12-14 14:10:02 -0500

    fixed PHP code for email setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once('index.html');
+
+//recipient
+$to = 'devinwiebelt@gmail.com';
+$to = $_POST['email'];
+
+$subject = 'Your $150 Promo is Here!';
+
+$name = $_POST['name'];
+$email= 'noreply@professionalpainters.com';
+$phone = $_POST['phone'];
+
+$message = 'Thank you for choosing Professional Painters! You are our number 1 customer!';
+
+$header = $email;
+
+if ($_POST){
+mail($to, $subject, $email, $message)
+};
