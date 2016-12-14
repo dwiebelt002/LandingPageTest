c1d0d2f8eb7b1db80bec6ebfa74c9026d55d94f2
Devin <devinwiebelt@gmail.com>
2016-12-14 14:22:11 -0500

    heroku push

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-<?php include_once('index.html');?>
+<?php include_once('index.html');
 
-// //recipient
-// $to = 'devinwiebelt@gmail.com';
-// $to = $_POST['email'];
+$to = 'devinwiebelt@gmail.com';
+$to = $_POST['email'];
 
-// $subject = 'Your $150 Promo is Here!';
+$subject = 'Your $150 Promo is Here!';
 
-// $name = $_POST['name'];
-// $email= 'noreply@professionalpainters.com';
-// $phone = $_POST['phone'];
+$name = $_POST['name'];
+$email= 'noreply@professionalpainters.com';
+$phone = $_POST['phone'];
 
-// $message = 'Thank you for choosing Professional Painters! You are our number 1 customer!';
+$message = 'Thank you for choosing Professional Painters! You are our number 1 customer!';
 
-// $header = $email;
+$header = $email;
 
-// if ($_POST){
-// mail($to, $subject, $email, $message);
-// }
+if ($_POST){
+mail($to, $subject, $email, $message);
+}
 
-// ?>
+?>
